52a34b70f76b0f508adcea381327f7899b6d05a7
Lekuru <levijordan007@gmail.com>
2023-06-23 18:39:36 +0200

    Add environment configuration

---
+++ config.py
@@ -0,0 +1,31 @@
+
+import dotenv
+import os
+
+dotenv.load_dotenv(override=False)
+
+POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+POSTGRES_PORT = int(os.environ.get('POSTGRES_PORT', 5432))
+POSTGRES_USER = os.environ.get('POSTGRES_USER')
+POSTGRES_HOST = os.environ.get('POSTGRES_HOST')
+
+S3_ACCESS_KEY = os.environ.get('S3_ACCESS_KEY')
+S3_SECRET_KEY = os.environ.get('S3_SECRET_KEY')
+S3_BASEURL    = os.environ.get('S3_BASEURL')
+
+REDIS_HOST = os.environ.get('REDIS_HOST')
+REDIS_PORT = int(os.environ.get('REDIS_PORT', 6379))
+
+WEB_HOST = os.environ.get('WEB_HOST')
+WEB_PORT = int(os.environ.get('WEB_PORT', 80))
+
+FREE_SUPPORTER = eval(os.environ.get('FREE_SUPPORTER', 'True').capitalize())
+ALLOW_RELAX = eval(os.environ.get('ALLOW_RELAX', 'True').capitalize())
+ENABLE_S3 = eval(os.environ.get('ENABLE_S3', 'True').capitalize())
+
+SCORE_RESPONSE_LIMIT = int(os.environ.get('SCORE_RESPONSE_LIMIT', 50))
+SCORE_SUBMISSION_KEY = os.environ.get('SCORE_SUBMISSION_KEY')
+
+DOMAIN_NAME = os.environ.get('DOMAIN_NAME')
+
+DATA_PATH = os.path.abspath('.data')
