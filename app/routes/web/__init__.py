68235bf1e4c0e9b93eee431538ca629e36b1bf7f
Lekuru <contact@lekuru.xyz>
2023-06-25 01:37:59 +0200

    Implement replays

--- app/routes/web/__init__.py
+++ app/routes/web/__init__.py
@@ -5,6 +5,7 @@ from . import leaderboards
 from . import screenshots
 from . import favourites
 from . import comments
+from . import replays
 from . import ratings
 from . import direct
 from . import error
@@ -16,6 +17,7 @@ router.include_router(screenshots.router)
 router.include_router(favourites.router)
 router.include_router(comments.router)
 router.include_router(ratings.router)
+router.include_router(replays.router)
 router.include_router(direct.router)
 router.include_router(error.router)
 router.include_router(title.router)
---
+++ app/routes/web/replays.py
@@ -0,0 +1,40 @@
+
+from fastapi import (
+    HTTPException,
+    APIRouter,
+    Response,
+    Query
+)
+
+router = APIRouter()
+
+import bcrypt
+import app
+
+@router.get('/osu-getreplay.php')
+def get_replay(
+    score_id: int = Query(..., alias='c'),
+    mode: int = Query(..., alias='m'),
+    username: str = Query(..., alias='u'),
+    password: str = Query(..., alias='h')
+):
+    if not (player := app.session.database.user_by_name(username)):
+        raise HTTPException(401)
+
+    if not bcrypt.checkpw(password.encode(), player.bcrypt.encode()):
+        raise HTTPException(401)
+
+    if not app.session.cache.user_exists(player.id):
+        raise HTTPException(401)
+
+    if not (score := app.session.database.score(score_id)):
+        raise HTTPException(404)
+
+    if score.status <= 0:
+        # Score is hidden
+        raise HTTPException(403)
+
+    if not (replay := app.session.storage.get_replay(score_id)):
+        raise HTTPException(404)
+
+    return Response(replay)
