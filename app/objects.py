ef3bf330da26d70354048f087f2a6e3431167755
Lekuru <contact@lekuru.xyz>
2023-06-26 22:41:19 +0200

    Add 'Chart' class

--- app/objects.py
+++ app/objects.py
@@ -8,6 +8,17 @@ from .common.objects import DBScore
 import hashlib
 import app
 
+class Chart(dict):
+    def entry(self, name: str, before, after):
+        self[f'{name}Before'] = str(before) if before is not None else ''
+        self[f'{name}After']  = str(after)  if after  is not None else ''
+
+    def get(self):
+        return self.__repr__()
+
+    def __repr__(self) -> str:
+        return "|".join(f"{str(k)}:{str(v)}" for k, v in self.items())
+
 class ClientHash:
     def __init__(self, md5: str, adapters: str, adapters_md5: str, uninstall_id: str, diskdrive_signature: str) -> None:
         self.diskdrive_signature = diskdrive_signature
