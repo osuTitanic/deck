ae808ebeb9611a918535fe07a66c54974df40acf
Lekuru <contact@lekuru.xyz>
2023-06-28 16:23:11 +0200

    Rename 'ss.py' to 'screenshot.py'

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,16 +2,16 @@
 from fastapi.responses import PlainTextResponse
 from fastapi import APIRouter
 
+from . import screenshot
 from . import avatar
 from . import web
-from . import ss
 
 import config
 
 router = APIRouter(default_response_class=PlainTextResponse)
+router.include_router(screenshot.router, prefix='/ss')
 router.include_router(avatar.router, prefix='/a')
 router.include_router(web.router, prefix='/web')
-router.include_router(ss.router, prefix='/ss')
 
 @router.get('/')
 def index():
