f01a3aff562bbbf175eda597602d30bdf339cbb3
Lekuru <contact@lekuru.xyz>
2023-07-29 02:36:38 +0200

    Fix legacy title image ???

--- app/routes/web/title.py
+++ app/routes/web/title.py
@@ -11,7 +11,7 @@ router = APIRouter()
 
 @router.get('/osu-title-image.php')
 def legacy_menu_icon(
-    image_checksum: Optional[str] = Query('', alias='c'),
+    # image_checksum: Optional[str] = Query('', alias='c'), why does this not work tf
     redirect: Optional[bool] = Query(False, alias='l')
 ):
     if redirect:
