77bb783ba0385026306ae19b83b08573fb844ea9
Lekuru <contact@lekuru.xyz>
2023-08-01 17:31:17 +0200

    Implement bancho_connect.php

--- app/routes/web/__init__.py
+++ app/routes/web/__init__.py
@@ -6,6 +6,7 @@ from . import screenshots
 from . import favourites
 from . import comments
 from . import scoring
+from . import connect
 from . import replays
 from . import ratings
 from . import direct
@@ -19,6 +20,7 @@ router.include_router(screenshots.router)
 router.include_router(favourites.router)
 router.include_router(comments.router)
 router.include_router(scoring.router)
+router.include_router(connect.router)
 router.include_router(ratings.router)
 router.include_router(replays.router)
 router.include_router(direct.router)
