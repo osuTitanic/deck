7ee941ce029995928fb471f4026297612fa1f664
Lekuru <contact@lekuru.xyz>
2023-08-08 01:38:44 +0200

    Migrate maps

--- app/routes/web/maps.py
+++ app/routes/web/maps.py
@@ -1,4 +1,6 @@
 
+from app.common.database.repositories import beatmaps
+
 from fastapi import (
     HTTPException,
     APIRouter
@@ -14,7 +16,7 @@ def index():
 
 @router.get('/maps/{filename}')
 def get_map(filename: str):
-    if not (beatmap := app.session.database.beatmap_by_file(filename)):
+    if not (beatmap := beatmaps.fetch_by_file(filename)):
         raise HTTPException(404)
 
     if not (file := app.session.storage.get_beatmap(beatmap.id)):
