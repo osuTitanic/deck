6ae11c4f0f94f0bbce8cb4bd08431634b9a5cc83
Lekuru <contact@lekuru.xyz>
2023-08-30 16:10:15 +0200

    Add "/osu-getstatus.php" support

---
+++ app/routes/web/status.py
@@ -0,0 +1,42 @@
+
+from app.common.database.repositories import beatmaps
+from app.common.constants import LegacyStatus
+
+from fastapi import (
+    HTTPException,
+    APIRouter,
+    Response,
+    Query
+)
+
+router = APIRouter()
+
+@router.get('/osu-getstatus.php')
+def get_beatmaps(
+    checksums: str = Query(..., alias='c')
+):
+    # Check amount of requests
+    if len(checksums := checksums.split(',')) > 60:
+        raise HTTPException(404)
+
+    # Check md5 size
+    if any([len(checksum) != 32 for checksum in checksums if checksum]):
+        raise HTTPException(400)
+
+    response = []
+
+    for checksum in checksums:
+        if not (beatmap := beatmaps.fetch_by_checksum(checksum)):
+            continue
+
+        status = 1 if beatmap.status > 0 else 0
+
+        response.append(','.join([
+            str(checksum),
+            str(status),
+            str(beatmap.id),
+            str(beatmap.set_id),
+            "" # beatmap.topic_id
+        ]))
+
+    return Response('\n'.join(response))
