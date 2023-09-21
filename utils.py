7d31b8e437b0bbfab4cb7650bc2c6247176b2842
Levi <contact@lekuru.xyz>
2023-09-21 10:11:54 +0200

    Add beatmap bucket and folder

--- utils.py
+++ utils.py
@@ -15,6 +15,7 @@ import os
 
 REQUIRED_BUCKETS = [
     'screenshots',
+    'beatmaps',
     'avatars',
     'replays',
     'assets',
@@ -103,6 +104,7 @@ def setup():
         # Create required folders if not they not already exist
         os.makedirs(f'{config.DATA_PATH}/images/achievements', exist_ok=True)
         os.makedirs(f'{config.DATA_PATH}/screenshots', exist_ok=True)
+        os.makedirs(f'{config.DATA_PATH}/beatmaps', exist_ok=True)
         os.makedirs(f'{config.DATA_PATH}/replays', exist_ok=True)
         os.makedirs(f'{config.DATA_PATH}/avatars', exist_ok=True)
 
