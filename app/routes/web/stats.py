51130a1f5898b8d6c8f0bcc51d60e83e3d328e92
Lekuru <contact@lekuru.xyz>
2023-12-16 15:30:21 +0100

    Implement `/osu-statoth.php` endpoint

---
+++ app/routes/web/stats.py
@@ -0,0 +1,43 @@
+
+from app.common.database.repositories import users
+from app.common.cache import status, leaderboards
+
+from fastapi import APIRouter, HTTPException, Query
+
+import hashlib
+import app
+
+router = APIRouter()
+
+@router.get('/osu-statoth.php')
+def legacy_user_stats(
+    username: str = Query(..., alias='u'),
+    checksum: str = Query(..., alias='c')
+):
+    app.session.logger.info(f'Got stats request for "{username}" ({checksum})')
+
+    # Validate checksum
+    checksum_match = hashlib.md5(f'{username}prettyplease!!!'.encode()).hexdigest()
+
+    if checksum != checksum_match:
+        app.session.logger.warning('Failed to send stats: Checksum mismatch!')
+        raise HTTPException(400)
+
+    if not (user_id := users.fetch_user_id(username)):
+        app.session.logger.warning('Failed to send stats: User not found!')
+        raise HTTPException(404)
+
+    if not (s := status.get(user_id)):
+        app.session.logger.warning('Failed to send stats: User not online!')
+        raise HTTPException(404)
+
+    current_rank = leaderboards.global_rank(user_id, s.mode.value)
+    current_acc = leaderboards.accuracy(user_id, s.mode.value)
+    current_score = leaderboards.score(user_id, s.mode.value)
+
+    return '|'.join([
+        str(current_score),
+        str(current_acc),
+        str(current_rank),
+        str(user_id) # Avatar Filename
+    ])
