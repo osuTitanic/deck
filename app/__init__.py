80e476d3fc1381fb4fcdb01a1f3e28fa73c6cf77
Lekuru <contact@lekuru.xyz>
2023-12-26 22:08:55 +0100

    Add docs url when debug mode is enabled

--- app/__init__.py
+++ app/__init__.py
@@ -37,8 +37,8 @@ api = FastAPI(
     title='Deck',
     description='API for osu! clients',
     version=config.VERSION,
-    redoc_url=None,
-    docs_url=None,
+    redoc_url=None if not config.DEBUG else '/redoc',
+    docs_url=None if not config.DEBUG else '/docs',
     debug=True if config.DEBUG else False
 )
 
