df9e5137606e63eeee2b3fc12ab6df6038a1f1f7
Lekuru <contact@lekuru.xyz>
2024-01-07 02:04:35 +0100

    Refactor updates endpoint

--- app/routes/release/update.py
+++ app/routes/release/update.py
@@ -1,20 +1,10 @@
 
+from app.common.helpers import clients
 from fastapi import APIRouter, Query
 
-import hashlib
 import app
 
 router = APIRouter()
-
-updates = [
-    {
-        'filename': 'osu!.exe',
-        'description': '',
-        'localfilename': 'osu!.exe',
-        'checksum': '9cbe9860afbfc39030c2c34c9434a98c',
-        'tests': ['extra']
-    }
-]
 
 @router.get('/update.php')
 def check_for_updates(
@@ -22,27 +12,47 @@ def check_for_updates(
     checksum: str = Query(..., alias='h'),
     ticks: int = Query(..., alias='t')
 ):
-    if not (file := app.session.storage.get_release_file(filename)):
+    if not (hashes := clients.get_client_hashes(filename)):
         return "0"
 
-    if checksum.lower() == hashlib.md5(file).hexdigest():
+    if checksum in hashes:
         return "0"
 
-    return "1"
+    # Patch filename structure: <filename>_<old_checksum>_<new_checksum>.patch
+    patches = [
+        file.removesuffix('.patch')
+        for file in app.session.storage.list('release')
+        if file.endswith('.patch')
+    ]
+
+    for patch in patches:
+        filename, old_checksum, new_checksum = patch.split('_')
+
+        if old_checksum != checksum:
+            continue
+
+        # Patch file was found
+        return "1"
+
+    return "0"
 
 @router.get('/update')
-def get_updates(ticks: int = Query(..., alias='time')):
-    return '\n'.join([
-        ' '.join([
-            update['filename'],
-            update['checksum'],
-            update['description'].replace(' ', '-'),
-            ','.join(update['tests']),
-            update['localfilename'].replace('\\', '/')
-        ])
-        for update in updates
-    ])
+def get_files(
+    ticks: int = Query(..., alias='t')
+):
+    # Respone format:
+    # <server_filename> <file_checksum> <description> <file_action> <old_checksum>\n (for each file)
+    # File action can be: "del", "noup", "zip" or "diff"
+    # "del" - delete file
+    # "noup" - only download file if it doesn't exist
+    # "zip" - download file and unzip it
+    # "diff" - download file and patch it
+    # TODO
+    ...
 
 @router.get('/patches.php')
 def patches():
-    return '\n'.join(app.session.storage.list('patches'))
+    return '\n'.join([
+        file for file in app.session.storage.list('release')
+        if file.endswith('.patch')
+    ])
