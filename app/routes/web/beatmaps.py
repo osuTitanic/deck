08d96465cb53a527ef8c5017ef7b42f9e181ebe9
Lekuru <contact@lekuru.xyz>
2024-05-08 16:55:38 +0200

    Implement `/osu-osz2-bmsubmit-getid.php`

--- app/routes/web/beatmaps.py
+++ app/routes/web/beatmaps.py
@@ -5,8 +5,9 @@ from typing import List, Callable, Tuple, Any
 from sqlalchemy.orm import Session
 from datetime import datetime
 
+from app.common.database import users, beatmapsets, beatmaps, topics
 from app.common.database.objects import DBUser, DBBeatmapset
-from app.common.database import users, beatmapsets, topics
+from app.common.helpers import beatmaps as beatmap_helper
 from app.common.cache import status
 
 from fastapi import (
@@ -56,18 +57,23 @@ def authenticate_user(
     player = users.fetch_by_name(username, session=session)
 
     if not player:
+        app.session.logger.warning(f'Failed to authenticate user: User not found.')
         return error_response(5, 'Authentication failed. Please check your login credentials.'), None
 
     if not bcrypt.checkpw(password.encode(), player.bcrypt.encode()):
+        app.session.logger.warning(f'Failed to authenticate user: Invalid password.')
         return error_response(5, 'Authentication failed. Please check your login credentials.'), None
 
     if player.silence_end and player.silence_end > datetime.now():
+        app.session.logger.warning(f'Failed to authenticate user: User is silenced.')
         return error_response(5, 'You are not allowed to upload beatmaps while silenced.'), None
 
     if player.restricted:
+        app.session.logger.warning(f'Failed to authenticate user: User is restricted.')
         return error_response(5, 'You are banned. Please contact support if you believe this is a mistake.'), None
 
     if not status.exists(player.id):
+        app.session.logger.warning(f'Failed to authenticate user: User is not connected to bancho.')
         return error_response(5, 'You are not connected to bancho, please try again!'), None
 
     return None, player
@@ -83,21 +89,50 @@ def is_bubbled(beatmapset: DBBeatmapset, session: Session) -> bool:
         if topic else False
     )
 
-def delete_upload_tickets(user: DBUser) -> None:
-    # TODO: Delete all upload tickets for the user
-    ...
+def delete_inactive_beatmaps(user: DBUser, session: Session = ...) -> None:
+    inactive_sets = beatmapsets.fetch_inactive(user.id)
+
+    app.session.logger.debug(f'Found {len(inactive_sets)} inactive beatmapsets.')
+
+    for set in inactive_sets:
+        # Delete beatmaps
+        beatmaps.delete_by_set_id(
+            set.id,
+            session=session
+        )
+
+    # Delete beatmapsets
+    beatmapsets.delete_inactive(
+        user.id,
+        session=session
+    )
 
 def remaining_beatmap_uploads(user: DBUser, session: Session) -> int:
     # TODO: Calculate the remaining beatmap uploads
-    ...
+    return 5
 
-def create_upload_ticket(user: DBUser, session: Session) -> int:
-    # TODO: Add a ticket to the cache
-    ...
+def create_beatmapset(user: DBUser, beatmap_ids: List[int], session: Session) -> Tuple[int, List[int]]:
+    # Create new beatmapset
+    set = beatmapsets.create(
+        id=beatmap_helper.next_beatmapset_id(session=session),
+        creator=user.name,
+        creator_id=user.id,
+        server=1
+    )
 
-def update_beatmap_ids(set_id: int, beatmap_ids: List[int], session: Session) -> List[int]:
-    # TODO: Validate & update the beatmap ids
-    ...
+    # Create beatmaps
+    new_beatmaps = [
+        beatmaps.create(
+            id=beatmap_helper.next_beatmap_id(session=session),
+            set_id=set.id,
+            session=session
+        )
+        for _ in beatmap_ids
+    ]
+
+    app.session.logger.info(f'Created new beatmapset ({set.id}) for user {user.name}.')
+
+    return set.id, [beatmap.id for beatmap in new_beatmaps]
 
 @router.get('/osu-osz2-bmsubmit-getid.php')
 def validate_upload_request(
@@ -109,15 +144,17 @@ def validate_upload_request(
     set_id: int = Query(..., alias='s')
 ) -> Response:
     if not config.OSZ2_SERVICE_URL:
+        app.session.logger.warning('The osz2-service url was not found. Aborting...')
         return error_response(5, 'Beatmap submission was disabled by an admin.')
 
     error, user = authenticate_user(username, password, session)
 
     if error:
+        # Failed to authenticate user
         return error
 
-    # Delete any previous upload tickets
-    delete_upload_tickets(user)
+    # Delete any inactive beatmaps
+    delete_inactive_beatmaps(user, session=session)
 
     remaining_beatmaps = remaining_beatmap_uploads(user, session)
     bubbled = False
@@ -125,39 +162,63 @@ def validate_upload_request(
     if (set_id > 0) and (beatmapset := beatmapsets.fetch_one(set_id, session)):
         # User wants to update an existing beatmapset
         if beatmapset.creator_id != user.id:
-            # User doesn't own this beatmapset
+            app.session.logger.warning(f'Failed to update beatmapset: User does not own the beatmapset.')
+            return error_response(1)
+
+        if beatmapset.server != 1:
+            app.session.logger.warning(f'Failed to update beatmapset: Beatmapset is not on Titanic.')
             return error_response(1)
 
         if beatmapset.status < -1:
-            # Beatmapset is in Graveyard
+            app.session.logger.warning(f'Failed to update beatmapset: Beatmapset is graveyarded.')
             return error_response(4)
 
         if beatmapset.status > 1:
-            # Beatmapset is already ranked
+            app.session.logger.warning(f'Failed to update beatmapset: Beatmapset is ranked or loved.')
             return error_response(3)
 
-        bubbled = is_bubbled(beatmapset, session)
+        bubbled = is_bubbled(
+            beatmapset,
+            session
+        )
+
+        beatmap_ids = [
+            beatmap.id
+            for beatmap in beatmapset.beatmaps
+        ]
+
+        app.session.logger.info(f'{user.name} wants to update a beatmapset ({set_id}).')
 
     else:
         # User wants to upload a new beatmapset
         if remaining_beatmaps <= 0:
+            app.session.logger.warning(f'Failed to create beatmapset: User has no remaining beatmap uploads.')
             return error_response(5, "You have reached your maximum amount of beatmaps you can upload.")
 
-        # Create a new beatmapset ticket inside the cache
-        set_id = create_upload_ticket(user, session)
+        # Create a new empty beatmapset inside the database
+        set_id, beatmap_ids = create_beatmapset(
+            user,
+            beatmap_ids,
+            session=session
+        )
 
-    # Either we don't have the file or the client has no package
+        if set_id is None:
+            return error_response(5, "An error occurred while creating the beatmapset.")
+
+    # Either we don't have the osz2 file or the client has no osz2 file
+    # If full-submit is true, the client will submit a patch file
     full_submit = (
         not app.session.storage.file_exists(f'{set_id}', 'osz2')
         or osz2_hash == '0'
     )
 
-    beatmap_ids = update_beatmap_ids(set_id, beatmap_ids, session)
+    # NOTE: In theory we could always set this to true, so we don't have
+    #       to store the osz2 files, but i've decided against this
 
     return Response('\n'.join([
         '0',
         f'{set_id}',
-        ','.join(beatmap_ids),
+        ','.join(map(str, beatmap_ids)),
         f'{int(full_submit)}',
         f'{int(bubbled)}'
     ]))
