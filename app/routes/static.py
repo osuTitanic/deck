6e7a4eef20a25ebf6b93da6480c0af9149e635e5
Lekuru <contact@lekuru.xyz>
2024-05-23 13:17:59 +0200

    Refactor `/d` request

--- app/routes/static.py
+++ app/routes/static.py
@@ -1,4 +1,6 @@
 
+from __future__ import annotations
+
 from fastapi.responses import StreamingResponse
 from fastapi import (
     HTTPException,
@@ -40,59 +42,23 @@ def mp3(filename: str):
     return Response(mp3)
 
 @router.get('/d/{id}')
-def osz(
-    id: str,
-    username: str = Query(..., alias='u'),
-    password: str = Query(..., alias='h')
-):
+def osz(id: str):
     if not id.replace('n', '').isdigit():
         raise HTTPException(400)
 
     set_id = int(id.replace('n', ''))
     no_video = 'n' in id
 
-    bundled_maps = [
-        3756,
-        163112,
-        140662,
-        151878,
-        190390,
-        123593
-    ]
-
-    # Skip user authentication for bundled maps
-    if not set_id in bundled_maps:
-        if not (user := users.fetch_by_name(username)):
-            raise HTTPException(401)
-
-        if not bcrypt.checkpw(password.encode(), user.bcrypt.encode()):
-            raise HTTPException(401)
-
+    # Fetch osz file
     if not (response := app.session.storage.api.osz(set_id, no_video)):
         raise HTTPException(404)
 
-    osz = response.iter_content(51200)
-
-    if filesize := response.headers.get('Content-Length'):
-        instance = app.session.database.session
-        instance.query(DBBeatmapset) \
-            .filter(DBBeatmapset.id == set_id) \
-            .update({
-                f'osz_filesize{"_novideo" if no_video else ""}': filesize,
-                'available': True
-            })
-        instance.commit()
-
-        if len(filesize) <= 0:
-            # Why does this happen?
-            raise HTTPException(500)
-
     return StreamingResponse(
-        osz,
+        response.iter_content(6400),
         media_type='application/octet-stream',
         headers={
             'Content-Disposition': f'attachment; filename={set_id}.osz',
-            'Content-Length': filesize or 0
+            'Content-Length': response.headers.get('Content-Length', 0)
         }
     )
 
