f2a6e2d5ad911227c926cef7dac3cf2ca2de5d46
Lekuru <contact@lekuru.xyz>
2024-07-18 15:01:08 +0200

    Implement topic ids for osu! direct

--- app/routes/web/direct.py
+++ app/routes/web/direct.py
@@ -16,7 +16,8 @@ from app.common.constants import DisplayMode
 from app.common.database.repositories import (
     beatmapsets,
     beatmaps,
-    users
+    users,
+    posts
 )
 
 import bcrypt
@@ -25,15 +26,18 @@ import app
 
 router = APIRouter()
 
-def online_beatmap(set: DBBeatmapset) -> str:
-    ratings = [r.rating for r in set.ratings]
-    avg_rating = (
-        sum(ratings) / len(ratings)
-        if ratings else 0
-    )
-
+def online_beatmap(set: DBBeatmapset, post_id: int) -> str:
     versions = ",".join(
         [f"{beatmap.version}@{beatmap.mode}" for beatmap in set.beatmaps]
+    )
+
+    ratings = [
+        r.rating for r in set.ratings
+    ]
+
+    average_rating = (
+        sum(ratings) / len(ratings)
+        if ratings else 0
     )
 
     status = {
@@ -52,16 +56,16 @@ def online_beatmap(set: DBBeatmapset) -> str:
         set.title   if set.title else "",
         set.creator if set.creator else "",
         status,
-        str(avg_rating),
+        str(average_rating),
         str(set.last_update),
         str(set.id),
-        str(set.id), # TODO: threadId
+        str(set.topic_id or 0),
         str(int(set.has_video)),
         str(int(set.has_storyboard)),
         str(set.osz_filesize),
         str(set.osz_filesize_novideo),
         versions,
-        str(set.id), # TODO: postId
+        str(post_id or 0),
     ])
 
 @router.get('/osu-search.php')
@@ -131,7 +135,8 @@ def search(
             ))
 
         for set in results:
-            response.append(online_beatmap(set))
+            post_id = posts.fetch_initial_post_id(set.topic_id, session)
+            response.append(online_beatmap(set, post_id))
     except Exception as e:
         app.session.logger.error(f'Failed to execute search: {e}', exc_info=e)
         return "-1\nServer error. Please try again!"
@@ -221,4 +226,7 @@ def pickup_info(
         }
     )
 
-    return online_beatmap(beatmapset)
+    return online_beatmap(
+        beatmapset,
+        posts.fetch_initial_post_id(beatmapset.topic_id, session)
+    )
