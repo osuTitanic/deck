2cefe2a79c7c74ed947a77bf070602d66fcf730a
Lekuru <contact@lekuru.xyz>
2025-01-02 02:41:05 +0100

    Use yielded session instead of managed session

--- app/routes/web/login.py
+++ app/routes/web/login.py
@@ -1,6 +1,7 @@
 
-from app.common.database import users
-from fastapi import APIRouter, Query
+from app.common.database.repositories import users
+from fastapi import APIRouter, Query, Depends
+from sqlalchemy.orm import Session
 from datetime import datetime
 
 import utils
@@ -10,27 +11,27 @@ router = APIRouter()
 
 @router.get('/osu-login.php')
 def legacy_login(
+    session: Session = Depends(app.session.database.yield_session),
     username: str = Query(...),
     password: str = Query(...)
 ) -> str:
-    with app.session.database.managed_session() as session:
-        if not (player := users.fetch_by_name(username, session=session)):
-            return "0"
+    if not (player := users.fetch_by_name(username, session=session)):
+        return "0"
 
-        if not utils.check_password(password, player.bcrypt):
-            return "0"
+    if not utils.check_password(password, player.bcrypt):
+        return "0"
 
-        if player.restricted or not player.activated:
-            return "0"
+    if player.restricted or not player.activated:
+        return "0"
 
-        users.update(
-            player.id,
-            {'latest_activity': datetime.now()},
-            session=session
-        )
+    users.update(
+        player.id,
+        {'latest_activity': datetime.now()},
+        session=session
+    )
 
-        app.session.logger.info(
-            f'Player "{player.name}" is about to connect to irc.'
-        )
+    app.session.logger.info(
+        f'Player "{player.name}" is about to connect to irc.'
+    )
 
-        return "1"
+    return "1"
