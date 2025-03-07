685ca3dfcd886707f08237dbb59ff4bd707c6b26
Lekuru <contact@lekuru.xyz>
2025-03-07 19:39:41 +0100

    Refactor language/genre updates

--- app/routes/web/beatmaps.py
+++ app/routes/web/beatmaps.py
@@ -561,6 +561,12 @@ def update_beatmap_metadata(
     detected_language = detect_language_from_tags(tags)
     detected_genre = detect_genre_from_tags(tags)
 
+    if beatmapset.genre_id <= 1:
+        beatmapset.genre_id = detected_genre.value
+
+    if beatmapset.language_id <= 1:
+        beatmapset.language_id = detected_language.value
+
     # Update beatmapset metadata
     beatmapsets.update(
         beatmapset.id,
@@ -573,8 +579,8 @@ def update_beatmap_metadata(
             'artist_unicode': metadata.get('ArtistUnicode'),
             'title_unicode': metadata.get('TitleUnicode'),
             'source_unicode': metadata.get('SourceUnicode'),
-            'genre_id': int(metadata.get('Genre', beatmapset.genre_id or detected_genre.value)),
-            'language_id': int(metadata.get('Language', beatmapset.language_id or detected_language.value)),
+            'genre_id': int(metadata.get('Genre', beatmapset.genre_id)),
+            'language_id': int(metadata.get('Language', beatmapset.language_id)),
             'has_video': any(ext in file_extensions for ext in video_file_extensions),
             'display_title': (
                 f'[bold:0,size:20]{metadata.get("Artist", "")}|'
