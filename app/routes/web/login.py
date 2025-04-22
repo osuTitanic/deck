ab2fb497aa3888b69feb9942439ab3bc8f294a08
Lekuru <contact@lekuru.xyz>
2025-04-22 20:06:28 +0200

    Add redis key for irc logins

--- app/routes/web/login.py
+++ app/routes/web/login.py
@@ -1,8 +1,10 @@
 
-from app.common.database.repositories import users
-from fastapi import APIRouter, Query, Depends
+from fastapi import APIRouter, Request, Query, Depends
 from sqlalchemy.orm import Session
 from datetime import datetime
+
+from app.common.database.repositories import users
+from app.common.helpers import ip
 
 import utils
 import app
@@ -11,6 +13,7 @@ router = APIRouter()
 
 @router.get('/osu-login.php')
 def legacy_login(
+    request: Request,
     session: Session = Depends(app.session.database.yield_session),
     username: str = Query(...),
     password: str = Query(...)
@@ -30,8 +33,15 @@ def legacy_login(
         session=session
     )
 
+    ip_address = ip.resolve_ip_adddress_fastapi(request)
+
+    app.session.redis.setex(
+        f"bancho:irc_login:{player.id}",
+        10, ip_address
+    )
+
     app.session.logger.info(
-        f'Player "{player.name}" is about to connect to irc.'
+        f'Player "{player.name}" is about to connect via. irc based osu!.'
     )
 
     return "1"
