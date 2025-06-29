e80b0d200b9e3283d8d4ee01f12bd2f0829d5516
Lekuru <contact@lekuru.xyz>
2025-06-30 00:58:31 +0200

    Don't return anything in `osu-checktweets` if downmessage is not set

--- app/routes/web/checktweets.py
+++ app/routes/web/checktweets.py
@@ -1,21 +1,19 @@
 
-from fastapi import APIRouter, Response
+from fastapi.responses import PlainTextResponse
+from fastapi import APIRouter
 import app
 
 router = APIRouter()
 
 @router.get('/osu-checktweets.php')
-def bancho_down_message() -> Response:
+def bancho_status_message() -> PlainTextResponse:
     """
     This endpoint was used to fetch tweets from @osustatus on twitter/x, which
     would be displayed on the client side. I will be using it here, to display
     a custom message that can be customized later on in the frontend.
     """
 
-    if (message := app.session.redis.get('bancho:downmessage')):
-        return Response(message)
+    if not (message := app.session.redis.get('bancho:statusmessage')):
+        return PlainTextResponse(status_code=200)
 
-    return Response(
-        'Bancho seems to have crashed. '
-        'Please contact an administrator, if this issue persists!'
-    )
+    return PlainTextResponse(message)
