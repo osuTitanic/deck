80ab73c95031ce5f524666dbc88ae82d9282ec5f
Lekuru <contact@lekuru.xyz>
2025-10-31 21:59:47 +0100

    Add support for beatmap IDs & checksums in `/web/maps/`

--- app/routes/web/maps.py
+++ app/routes/web/maps.py
@@ -1,21 +1,18 @@
 
+from fastapi import HTTPException, APIRouter, Response
+from app.common.database.objects import DBBeatmap
 from app.common.database import beatmaps
 from urllib.parse import quote
-from fastapi import (
-    HTTPException,
-    APIRouter,
-    Response
-)
 
 import app
 
 router = APIRouter()
 
-@router.get('/maps/{filename}')
-def beatmap_file(filename: str) -> Response:
-    app.session.logger.info(f'Got map request for: "{filename}".')
+@router.get('/maps/{query}')
+def beatmap_file(query: str) -> Response:
+    app.session.logger.info(f'Got map request for: "{query}".')
 
-    if not (beatmap := beatmaps.fetch_by_file(filename)):
+    if not (beatmap := resolve_beatmap(query)):
         raise HTTPException(404)
 
     if not (file := app.session.storage.get_beatmap(beatmap.id)):
@@ -25,6 +22,15 @@ def beatmap_file(filename: str) -> Response:
         content=file,
         media_type='application/octet-stream',
         headers={
-            'Content-Disposition': f'attachment; filename="{quote(filename)}"'
+            'Content-Disposition': f'attachment; filename="{quote(beatmap.filename)}"'
         }
     )
+
+def resolve_beatmap(query: str) -> DBBeatmap | None:
+    if query.isdigit():
+        return beatmaps.fetch_by_id(int(query))
+
+    if query.endswith('.osu'):
+        return beatmaps.fetch_by_file(query)
+
+    return beatmaps.fetch_by_checksum(query)
