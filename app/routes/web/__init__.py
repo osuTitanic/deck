adf5545cb1dfa2f4732f050b1b3581ff0da2e2ef
Lekuru <contact@lekuru.xyz>
2025-11-22 20:18:55 +0100

    Add `/web/osu-markasread.php` endpoint implementation

--- app/routes/web/__init__.py
+++ app/routes/web/__init__.py
@@ -5,6 +5,7 @@ from . import leaderboards
 from . import screenshots
 from . import checktweets
 from . import beatmapinfo
+from . import markasread
 from . import favourites
 from . import benchmark
 from . import beatmaps
@@ -30,6 +31,7 @@ router.include_router(leaderboards.router)
 router.include_router(screenshots.router)
 router.include_router(checktweets.router)
 router.include_router(beatmapinfo.router)
+router.include_router(markasread.router)
 router.include_router(favourites.router)
 router.include_router(benchmark.router)
 router.include_router(beatmaps.router)
---
+++ app/routes/web/markasread.py
@@ -0,0 +1,41 @@
+
+from fastapi import HTTPException, Response, APIRouter, Query, Depends
+from sqlalchemy.orm import Session
+
+from app.common.database import messages, users
+from app import session, utils
+
+router = APIRouter()
+
+@router.get("/osu-markasread.php")
+def mark_channel_as_read(
+    session: Session = Depends(session.database.yield_session),
+    username: str = Query(..., alias="u"),
+    password: str = Query(..., alias="h"),
+    channel: str = Query(...)
+) -> Response:
+    if not (player := users.fetch_by_name(username, session=session)):
+        raise HTTPException(401)
+
+    if not utils.check_password(password, player.bcrypt):
+        raise HTTPException(401)
+
+    if channel.startswith('#'):
+        # We don't support marking public channels as read right now
+        return Response(status_code=200)
+
+    safe_name = channel.strip().replace(' ', '_').lower()
+
+    if not (target_user := users.fetch_by_safe_name(safe_name, session=session)):
+        raise HTTPException(404)
+
+    total_messages = messages.update_private_all(
+        target_user.id,
+        player.id,
+        {'read': True},
+        session=session
+    )
+    session.logger.info(
+        f"'{player.name}' marked {total_messages} messages as read in DMs with '{target_user.name}'"
+    )
+    return Response(status_code=200)
