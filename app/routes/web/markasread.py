53cc26c871d5201213dde9f3baca9e205c47b27a
Lekuru <contact@lekuru.xyz>
2025-11-22 20:24:44 +0100

    Fix `session` keyword usage

--- app/routes/web/markasread.py
+++ app/routes/web/markasread.py
@@ -9,12 +9,12 @@ router = APIRouter()
 
 @router.get("/osu-markasread.php")
 def mark_channel_as_read(
-    session: Session = Depends(session.database.yield_session),
+    database: Session = Depends(session.database.yield_session),
     username: str = Query(..., alias="u"),
     password: str = Query(..., alias="h"),
     channel: str = Query(...)
 ) -> Response:
-    if not (player := users.fetch_by_name(username, session=session)):
+    if not (player := users.fetch_by_name(username, session=database)):
         raise HTTPException(401)
 
     if not utils.check_password(password, player.bcrypt):
@@ -26,14 +26,14 @@ def mark_channel_as_read(
 
     safe_name = channel.strip().replace(' ', '_').lower()
 
-    if not (target_user := users.fetch_by_safe_name(safe_name, session=session)):
+    if not (target_user := users.fetch_by_safe_name(safe_name, session=database)):
         raise HTTPException(404)
 
     total_messages = messages.update_private_all(
         target_user.id,
         player.id,
         {'read': True},
-        session=session
+        session=database
     )
     session.logger.info(
         f"'{player.name}' marked {total_messages} messages as read in DMs with '{target_user.name}'"
