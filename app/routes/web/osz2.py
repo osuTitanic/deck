c22a7648ed61922f310d24fbda032beea4573828
Lekuru <contact@lekuru.xyz>
2025-12-06 23:21:03 +0100

    Use "5" error code for all errors in osz2 endpoints

--- app/routes/web/osz2.py
+++ app/routes/web/osz2.py
@@ -23,6 +23,37 @@ def get_osz2_hashes(
         beatmapset.meta_hash
     )
 
+@router.get("/osu-osz2-getfileinfo.php")
+def get_osz2_file_info(
+    session: Session = Depends(app.session.database.yield_session),
+    username: str = Query(..., alias="u"),
+    password: str = Query(..., alias="h"),
+    set_id: int = Query(..., alias="s")
+) -> Response:
+    player = users.fetch_by_name(username, session=session)
+
+    if not player:
+        return error_response(5, 'Authentication failed. Please check your login credentials.')
+
+    if not app.utils.check_password(password, player.bcrypt):
+        return error_response(5, 'Authentication failed. Please check your login credentials.')
+
+    if not (osz2 := app.session.storage.get_osz2_internal(set_id)):
+        return error_response(5)
+
+    try:
+        package = Osz2Package.from_bytes(osz2)
+    except Exception as e:
+        app.logger.warning(f"Failed to read osz2 package: {e}")
+        return error_response(5)
+
+    info_strings = [
+        f"{file.filename}:{file.offset}:{file.size}:{file.hash.hex().upper()}:"
+        f"{int(file.date_created.timestamp())}:{int(file.date_modified.timestamp())}"
+        for file in package.files
+    ]
+    return "|".join(info_strings) + "\n"
+
 @router.get("/osu-osz2-getrawheader.php")
 def get_osz2_header(
     session: Session = Depends(app.session.database.yield_session),
@@ -39,13 +70,13 @@ def get_osz2_header(
         return error_response(5, 'Authentication failed. Please check your login credentials.')
 
     if not (osz2 := app.session.storage.get_osz2_internal(set_id)):
-        return error_response(1)
+        return error_response(5)
 
     try:
         package = Osz2Package.from_bytes(osz2)
     except Exception as e:
         app.logger.warning(f"Failed to read osz2 package: {e}")
-        return error_response(1)
+        return error_response(5)
 
     return Response(osz2[0 : package.data_offset])
 
@@ -66,49 +97,18 @@ def get_osz2_file_contents(
         return error_response(5, 'Authentication failed. Please check your login credentials.')
 
     if not (osz2 := app.session.storage.get_osz2_internal(set_id)):
-        return error_response(1)
+        return error_response(5)
 
     try:
         package = Osz2Package.from_bytes(osz2)
     except Exception as e:
         app.logger.warning(f"Failed to read osz2 package: {e}")
-        return error_response(1)
+        return error_response(5)
 
     if not (file := package.find_file_by_name(filename)):
-        return error_response(1)
+        return error_response(5)
 
     return Response(file.content)
-
-@router.get("/osu-osz2-getfileinfo.php")
-def get_osz2_file_info(
-    session: Session = Depends(app.session.database.yield_session),
-    username: str = Query(..., alias="u"),
-    password: str = Query(..., alias="h"),
-    set_id: int = Query(..., alias="s")
-) -> Response:
-    player = users.fetch_by_name(username, session=session)
-
-    if not player:
-        return error_response(5, 'Authentication failed. Please check your login credentials.')
-
-    if not app.utils.check_password(password, player.bcrypt):
-        return error_response(5, 'Authentication failed. Please check your login credentials.')
-
-    if not (osz2 := app.session.storage.get_osz2_internal(set_id)):
-        return error_response(1)
-
-    try:
-        package = Osz2Package.from_bytes(osz2)
-    except Exception as e:
-        app.logger.warning(f"Failed to read osz2 package: {e}")
-        return error_response(1)
-
-    info_strings = [
-        f"{file.filename}:{file.offset}:{file.size}:{file.hash.hex().upper()}:"
-        f"{int(file.date_created.timestamp())}:{int(file.date_modified.timestamp())}"
-        for file in package.files
-    ]
-    return "|".join(info_strings) + "|"
 
 @router.get("/osu-magnet.php")
 def get_osu_magnet(
