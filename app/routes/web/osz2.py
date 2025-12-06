0588e72e0436a8262320a3da5a5ed41e9fc343d5
Lekuru <contact@lekuru.xyz>
2025-12-06 22:18:53 +0100

    Add `/osu-osz2-getfilecontents.php` endpoint

--- app/routes/web/osz2.py
+++ app/routes/web/osz2.py
@@ -22,3 +22,36 @@ def get_osz2_hashes(
         beatmapset.body_hash,
         beatmapset.meta_hash
     )
+
+@router.get("osu-osz2-getfilecontents.php")
+def get_osz2_file_contents(
+    session: Session = Depends(app.session.database.yield_session),
+    username: str = Query(..., alias="u"),
+    password: str = Query(..., alias="h"),
+    set_id: int = Query(..., alias="s"),
+    filename: str = Query(..., alias="f")
+) -> Response:
+    player = users.fetch_by_name(username, session=session)
+
+    if not player:
+        return error_response(5, 'Authentication failed. Please check your login credentials.')
+
+    if not app.utils.check_password(password, player.bcrypt):
+        return error_response(5, 'Authentication failed. Please check your login credentials.')
+
+    if not (beatmapset := beatmapsets.fetch_one(set_id, session)):
+        return error_response(1)
+    
+    if not (osz2 := app.session.storage.get_osz2_internal(beatmapset.id)):
+        return error_response(1)
+
+    try:
+        package = Osz2Package.from_bytes(osz2)
+    except Exception as e:
+        app.logger.warning(f"Failed to read osz2 package: {e}")
+        return error_response(1)
+    
+    if not (file := package.find_file_by_name(filename)):
+        return error_response(1)
+
+    return Response(file.content)
