e8c884e1c93aaaec67a1c6a48f71ffda25604ae3
Lekuru <contact@lekuru.xyz>
2025-12-28 02:03:05 +0100

    Add catch-all exception handler for fastapi

--- app/exceptions.py
+++ app/exceptions.py
@@ -17,15 +17,20 @@ def exception_handler(request: Request, exc: HTTPException):
 
 @api.exception_handler(StarletteHTTPException)
 def exception_handler(request: Request, exc: StarletteHTTPException):
+    headers = exc.headers if exc.headers else {}
+    headers.update({'detail': exc.detail})
+
     return Response(
         status_code=exc.status_code,
-        headers={'detail': exc.detail}
+        headers=headers
     )
 
 @api.exception_handler(RequestValidationError)
 def validation_error(request: Request, exc: RequestValidationError):
     session.logger.error(f"Validation error: {exc.errors()}")
-    return Response(
-        status_code=400,
-        content='no'
-    )
+    return Response(status_code=400, content="")
+
+@api.exception_handler(Exception)
+def catch_all_exception_handler(request: Request, exc: Exception):
+    session.logger.error(f"Unhandled exception: {exc}")
+    return Response(status_code=500, content="")
