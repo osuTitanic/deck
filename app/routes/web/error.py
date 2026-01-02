5150a7512401e2a38fb9bf4284a4dd50abc2afd7
Lekuru <contact@lekuru.xyz>
2026-01-02 22:12:57 +0100

    Refactor & cleanup `osu-error.php` endpoint

--- app/routes/web/error.py
+++ app/routes/web/error.py
@@ -10,22 +10,12 @@ from fastapi import (
     Form
 )
 
-router = APIRouter()
-
 import json
 import app
 
-def parse_osu_config(config: str) -> Dict[str, str]:
-    if config.count('\n') > 2**8:
-        return {}
-
-    return {
-        k.strip():v.strip()
-        for (k, v) in [
-            line.split('=', 1) for line in config.splitlines()
-            if '=' in line and not line.startswith('#')
-        ]
-    }
+router = APIRouter()
+ignored_feedback = ('update error',)
+flagged_skins = ('taikomania', 'duda skin', 'arpia97')
 
 @router.post('/osu-error.php')
 def osu_error(
@@ -46,38 +36,22 @@ def osu_error(
     exception: str = Form(...),
     version: str = Form(...)
 ) -> Response:
-    ignored_feedback = [
-        'update error',
-        # TODO Add more
-    ]
-
     if feedback.lower() in ignored_feedback:
         return Response(status_code=200)
 
-    # Parse config to get password
-    config = parse_osu_config(config)
-
     if not (user := users.fetch_by_id(user_id, session=session)):
-        return Response(status_code=200)
-
-    if not app.utils.check_password(config.get('Password', ''), user.bcrypt):
         return Response(status_code=200)
 
     if user.restricted or not user.activated:
         return Response(status_code=200)
 
-    flagged_skins = [
-        'taikomania',
-        'duda skin',
-        'arpia97'
-    ]
-
-    skin_name = config.get('Skin', '').lower()
+    osu_config = parse_osu_config(config)
+    skin_name = osu_config.get('Skin', '').lower()
 
     # Check if player is using a flagged skin
     if any(name in skin_name for name in flagged_skins):
         officer.call(
-            f'"{user.name}" is using a flagged skin: "{config["Skin"]}"',
+            f'"{user.name}" is using a flagged skin: "{osu_config["Skin"]}"',
         )
 
     error_dict = {
@@ -89,10 +63,21 @@ def osu_error(
         'stacktrace': stacktrace,
         'exehash': exehash
     }
-
     app.session.logger.warning(
         f'Client error from "{username}":\n'
         f'{json.dumps(error_dict, indent=4)}'
     )
 
     return Response(status_code=200)
+
+def parse_osu_config(config: str) -> Dict[str, str]:
+    if config.count('\n') > 2**8:
+        return {}
+
+    return {
+        k.strip():v.strip()
+        for (k, v) in [
+            line.split('=', 1) for line in config.splitlines()
+            if '=' in line and not line.startswith('#')
+        ]
+    }
