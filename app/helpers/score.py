7cbccfce4ec62ec68375f64c77f98db259b19224
Riley <145041122+Riley-3@users.noreply.github.com>
2026-08-14 14:58:04 -0500

    Add ScoreSecurityHash and ScoreNetworkAdapter

--- app/helpers/score.py
+++ app/helpers/score.py
@@ -25,6 +25,7 @@ MAX_PROCESS_LINE_LENGTH = 2048 # Prevent processing long lines
 MAX_PROCESS_NUM_LINES = 1000 # Prevent processing too many lines
 PROCESS_LIST_LEFT_PATTERN = re.compile(r"^([a-fA-F0-9]{32})\s+(.*)") # checked for redos
 
+# <md5> <full_path> | <process_name> (<window_title>)
 class ScoreProcess:
     def __init__(
         self,
@@ -53,7 +54,6 @@ class ScoreProcess:
                 process_name=None, 
                 window_title=None)
 
-        # <md5> <full_path> | <process_name> (<window_title>)
         line = line.strip()
 
         # using a full regex here is prone to a ReDoS because of | and ( character backtracks
@@ -75,7 +75,7 @@ class ScoreProcess:
 
         left, right = line.split(" | ", 1)
         match_left = PROCESS_LIST_LEFT_PATTERN.match(left)
-        
+
         if match_left:
             md5 = match_left.group(1)
             full_path = match_left.group(2)
@@ -97,6 +97,90 @@ class ScoreProcess:
             full_path=full_path, 
             process_name=process_name,
             window_title=window_title
+        )
+
+# AABBCCDDEEFF.AABBCCDDEEFF.AABBCCDDEEFF.. (empty is fine, but normally 6 bytes uppercase)
+class ScoreNetworkAdapter():
+    def __init__(
+        self,
+        physical_address: str | None
+    ) -> None:
+        self.physical_address = physical_address
+
+    @classmethod
+    def from_adapter_list (cls, part: str | None) -> list[ScoreNetworkAdapter] | None:
+        if part is None:
+            return None
+        
+        return [cls(physical_address=addr) for addr in part.split(".")]
+
+# <osu!.exe md5>:<MAC Address[0].MAC Address[1]>:<MAC Address combined md5>:<UniqueId md5>:<UniqueId2 md5>:
+class ScoreSecurityHash:
+    def __init__(
+        self,
+        osu_exe_md5: str | None,
+        network_adapters: list[ScoreNetworkAdapter] | None,
+        network_adapters_md5: str | None,
+        unique_id_md5: str | None,
+        unique_id2_md5: str | None
+    )
+        self.osu_exe_md5 = osu_exe_md5
+        self.network_adapters = network_adapters
+        self.network_adapters_md5 = network_adapters_md5
+        self.unique_id_md5 = unique_id_md5
+        self.unique_id2_md5 = unique_id2_md5
+
+    @staticmethod
+    def validate_adapters_unprocessed(network_adapters_str: str, network_adapters_md5: str) -> bool:
+        """Checks if md5(network_adapters_str) == network_adapters_md5"""
+        return hashlib.md5(network_adapters_md5).hexdigest() == network_adapters_md5 # we can expect the network_adapters_md5 to be lowercase coming from the client
+
+    def validate_adapters (self) -> bool:
+        """Runs a check to make sure network_adapters_md5 is a real md5 of the network_adapters"""
+        if network_adapters is None:
+            return True
+        
+        if len(network_adapters) == 0:
+            return True
+        
+        # We do an early check here because when we reconstruct the adapters using this value
+        # we would get "runningunderwine." instead of "runningunderwine", causing the md5 to mismatch anyway.
+        if network_adapters[0] == "runningunderwine": 
+            return True
+
+        network_adapter_str = "".join(f"{network_adapter.physical_address  or ''}." for adapter in self.network_adapters)
+        return ScoreSecurityHash.validate_adapters_unprocessed(network_adapter_str, self.network_adapters_md5)
+
+    @classmethod
+    def from_string (cls, security_hash_str: str | None) -> ScoreSecurityHash | None:
+        if part is None:
+            return None
+        
+        osu_exe_md5 = None
+        network_adapters = None
+        network_adapters_md5 = None
+        unique_id_md5 = None
+        unique_id2_md5 = None
+
+        parts = security_hash_str.split(":")
+        for i, part in enumerate(parts[:5]): # we can't expect it to be uniform since there are hints that this was smaller (3 parts) at one point.
+            if i == 0:
+                osu_exe_md5 = part
+            elif i == 1:
+                network_adapters = ScoreNetworkAdapter.from_adapter_list(part)
+            elif i == 2:
+                network_adapters_md5 = part
+            elif i == 3:
+                unique_id_md5 = part
+            elif i == 4:
+                unique_id2_md5 = part
+        
+        return cls(
+            osu_exe_md5=osu_exe_md5,
+            network_adapters=network_adapters
+            network_adapters_md5=network_adapters_md5,
+            unique_id_md5=unique_id_md5
+            unique_id2_md5=unique_id2_md5
         )
 
 class Score:
